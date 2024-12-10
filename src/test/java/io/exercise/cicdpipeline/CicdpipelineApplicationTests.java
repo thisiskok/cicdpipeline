0f3c8ecedc013f0315b270d6bb7ea9d4dc7f1f31
Yongjie <yongjie@luday.se>
2024-12-11 00:30:05 +0100

    initial commit

---
+++ src/test/java/io/exercise/cicdpipeline/CicdpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.exercise.cicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
