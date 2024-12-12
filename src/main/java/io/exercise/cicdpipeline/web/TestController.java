a9907b50949cdd7599e6e2fc9f235eeb9e05bc39
Yongjie <yongjie@luday.se>
2024-12-12 16:02:35 +0100

    updated controller

--- src/main/java/io/exercise/cicdpipeline/web/TestController.java
+++ src/main/java/io/exercise/cicdpipeline/web/TestController.java
@@ -8,11 +8,16 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping(path = "/test")
+//@RequestMapping(path = "/test")
 public class TestController {
 
     @GetMapping
+    public ResponseEntity<Map<String, String>> healthcheck(){
+        return ResponseEntity.ok().body(Map.of("Status", "Up"));
+    }
+
+    @GetMapping("/test")
     public ResponseEntity<Map<String, String>> test(){
-        return ResponseEntity.ok().body(Map.of("Testing", "Up and running"));
+        return ResponseEntity.ok().body(Map.of("Testing", "Test Data"));
     }
 }
